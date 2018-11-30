f896ec029358f576642463e1fa627a282a0adf9b
mariamosmann <rossimosmann@gmail.com>
2018-11-30 14:06:57 -0500

    progress! :octocat:

--- src/UserList.js
+++ src/UserList.js
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const UserList = (props) => { 
-    const taskDone = function () {
+class UserList extends Component { 
+    // CONSTRUCTOR START
+    constructor() {
+        super();
+        this.state = {
+            
+            taskComplete: false
+        }
+    }
+    // CONSTRUCTOR END
+
+    //FUNCTIONS START
+    taskDone = (event) => {
         alert("You did it! Keep up the good work!");
+
+        const thisTask = event.target.id 
+
+        console.log(thisTask)
+
+        if(this.state.taskComplete == false) {
+            this.setState({
+                taskComplete: true
+            })
+        } else if (this.state.taskComplete == true) {
+            this.setState({
+                taskComplete: false
+            })
+        }            
     }
     
-    const display = function () {
-        if(props.doable1 != "") {
+    display = () => {
+        if(this.props.doable1 != "") {
             return (
             <div className="userList__wrapper wrapper">
                 <h2>User List</h2>
 
-                    <p className="userList__text" onClick={taskDone}>{props.doable1} <span className="userList__span"><i className="userList__icon far fa-square"></i>
+                    <p id="doable1" className="userList__text" onClick={this.taskDone}>{this.props.doable1} <span className="userList__span">
+                    <i className={
+                        this.state.taskComplete
+                        ? 
+                        "userList__icon far fa-check-square"
+                         :
+                        "userList__icon far fa-square"
+                    }></i>
                     </span>
                     </p>
 
-                    <p className="userList__text" onClick={taskDone}>{props.doable2} <span className="userList__span"><i className="userList__icon far fa-square"></i></span></p>
+                    <p id="doable2" className="userList__text" onClick={this.taskDone}>{this.props.doable2} <span className="userList__span"><i className={
+                        this.state.taskComplete
+                            ?
+                            "userList__icon far fa-check-square"
+                            :
+                            "userList__icon far fa-square"
+                    }></i>
+                    </span>
+                    </p>
 
-                    <p className="userList__text" onClick={taskDone}>{props.dailyGoal} <span className="userList__span"><i className="userList__icon far fa-square"></i></span></p>
+                    <p id="dailyGoal" className="userList__text" onClick={this.taskDone}>{this.props.dailyGoal} <span className="userList__span"><i className={
+                        this.state.taskComplete
+                            ?
+                            "userList__icon far fa-check-square"
+                            :
+                            "userList__icon far fa-square"
+                    }></i>
+                    </span>
+                    </p>
 
                 <input
-                    onClick={props.resetList}
+                    onClick={this.props.resetList}
                     type="submit"
                     value="Reset the list" className="userList__resetListButton"
                 />
@@ -33,10 +81,15 @@ const UserList = (props) => {
             )
         }
     } 
+    //FUNCTIONS END
     
-    return (
-        display()
-    )
+    // RENDER START
+    render() {
+        return (
+            this.display()
+        )
+    }
+    // RENDER END
 }
 
 export default UserList;
