57b39f4b5e6fe14b5955a3ea74afdd51fdf029fe
mariamosmann <rossimosmann@gmail.com>
2019-01-02 21:06:12 -0500

    added links, improved text, work on styles :octocat:

--- src/components/About.js
+++ src/components/About.js
@@ -46,13 +46,48 @@ class About extends Component {
 
                     <p className="about__text">This project was developed while I was in <a href="https://hackeryou.com/" target="_blank" className="about__link">HackerYou</a> bootcamp. As we were approaching our final weeks everyone was feeling anxious and overwhelmed. The amount of coffee intake was growing exponentially, while the sleeping hours were decreasing by the same rate. I, a normally positive and cheerful person, was feeling down and terrably anxious. And then it hit me: excess of caffeine and lack of sleep! My triggers. I knew exactly how to go back to my normal self, and what kind of project I wanted to build next.</p>
 
-                    <p className="about__text">When I started the program I wanted to develop a project that I could keep working on after bootcamp. Something useful in the real world and that I cared about. Experiencing anxiety again reminded me how terrible it can be, and how simple habits can rewire your brain in the best way possible. I used to be a very anxious person. I also have friends who are affected by Anxiety Disorders, so if it was awful for me, I can only imagine what is like for them. In an effort to help them better, and also help me, I started researching about anxiety and what can be done to alleviate that.</p>
+                    <p className="about__text">When I started the program I wanted to develop a project that I could keep working on after bootcamp. Something useful in the real world and that I cared about. Experiencing anxiety again reminded me how terrible it can be, and how simple habits can rewire our brain in the best way possible. I used to be a very anxious person. I also have friends who are affected by Anxiety Disorders, so if it was awful for me, I could only imagine what is like for them. In an effort to help them better, and also help me, I started researching about anxiety and what can be done to alleviate that.</p>
 
-                    <p className="about__text">There are plenty of tools, some work better than the others, it's a really personal matter and I advise trying them all. As this project develops I'll add some of my favourites.</p>
+                    <p className="about__text">I came across different tools, and through trial and error learned which worked better for me. There are plenty of them, it's a really personal matter, so I advise trying as many as you can. As this project develops I'll add some of my favourites.</p>
 
                     <p className="about__text">If you're interested in collaborating in this project, please contact me! This is a work in progress, as we all should be.</p>
 
-                    
+                    <ul class="about__socialmedia socialmedia">
+                        <li class="socialmedia__item">
+                            <a href="http://www.tuni.tech" class="socialmedia__link">
+                                <span class="sr-only">Tuni's site.</span>
+                                <i class="fas fa-tv"></i>
+                            </a>
+                        </li>
+
+                            <li class="socialmedia__item">
+                                <a href="mailto:tuni.webdev@gmail.com" class="socialmedia__link">
+                                    <span class="sr-only">Tuni's Email.</span>
+                                    <i class="far fa-envelope"></i>
+                                </a>
+                            </li>                            
+
+                            <li class="socialmedia__item">
+                                <a href="https://www.linkedin.com/in/tuni/" target="_blank" class="socialmedia__link">
+                                    <span class="sr-only">Tuni's LinkedIn.</span>
+                                    <i class="fab fa-linkedin-in socialmedia__icon"></i>
+                                </a>
+                            </li>                            
+                        
+                            <li class="socialmedia__item">
+                                <a href="https://twitter.com/tuni_tech" target="_blank" class="socialmedia__link">
+                                    <span class="sr-only">Tuni's Twitter.</span>
+                                    <i class="fab fa-twitter socialmedia__icon"></i>
+                                </a>
+                            </li>
+                        
+                            <li class="socialmedia__item">
+                                <a href="https://github.com/tunimosmann" target="_blank" class="socialmedia__link">
+                                    <span class="sr-only socialmedia__icon">Tuni's GitHub.</span>
+                                    <i class="fab fa-github"></i>
+                                </a>
+                            </li>
+                        </ul>
 
                     {/* MORE INFO START */}
                     <div className="about__moreInfo moreInfo">
